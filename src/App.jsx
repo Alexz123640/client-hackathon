797713290cd92aaee7595bf3bcc58fc3a006a55a
Alex Cordova <developer2@mbww.com>
2024-11-08 23:45:17 -0500

    Init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Menu from "./components/Menu";
+
+function App() {
+  return (
+    <>
+      <div className="relative h-[100vh]">
+        <div className=""></div>
+        <Menu />
+      </div>
+    </>
+  );
+}
+
+export default App;
