0750caa5e37f9ac1c65cd8ffccf0d284d8b6f957
Alex Cordova <u19101608@utp.edu.pe>
2024-11-09 10:38:24 -0500

    API

--- src/Pages/Mapa.jsx
+++ src/Pages/Mapa.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+} from "@vis.gl/react-google-maps";
 
 const Mapa = () => {
+  const position = { lat: -16.398829, lng: -71.5394728 };
+
   return (
-    <div>
-      <div>MAPA</div>
-      
-    </div>
+    <>
+      <APIProvider key='AIzaSyBGZcIQ5-5IKAgM513s5CVC9msfiFjt7Rg'>
+        <div>MAPA</div>
+      </APIProvider>
+    </>
   );
-}
+};
 
 export default Mapa;
