d323bbe425681c141bf324e29b889e78bda21f7d
EdsonDiegoQuispeRodriguez <U19220103@utp.edu.pe>
2024-11-09 12:20:12 -0500

    [Hackathon] Add the map with example.

--- src/Pages/Mapa.jsx
+++ src/Pages/Mapa.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   APIProvider,
@@ -12,8 +14,22 @@ const Mapa = () => {
 
   return (
     <>
-      <APIProvider key='AIzaSyBGZcIQ5-5IKAgM513s5CVC9msfiFjt7Rg'>
-        <div>MAPA</div>
+      <APIProvider apiKey='AIzaSyBGZcIQ5-5IKAgM513s5CVC9msfiFjt7Rg'>
+        <div style={{height: "100vh", width: "100%"}}> 
+          <Map 
+            zoom={9} 
+            center={position} 
+            mapId={process.env.NEXT_PUBLIC_MAP_ID}
+            >
+              <AdvancedMarker position={position}>
+                <Pin
+                  background={"grey"}
+                  borderColor={"green"}
+                  glyphColor={"purple"}
+                ></Pin>
+              </AdvancedMarker>
+          </Map>
+        </div>
       </APIProvider>
     </>
   );
